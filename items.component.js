b98317b9ca092bcc7037bee30ee32d27aa4a9443
Lokesh-Bongu <lokesh.bongu@tpsouthernodisha.com>
2024-03-29 11:35:32 +0530

    Web File

--- items.component.js
+++ items.component.js
@@ -7,9 +7,7 @@
     ItemsController.$inject = ['items'];
     function ItemsController(items) {
         var itemsCtrl = this;
-        console.log(".items",items)
         itemsCtrl.items = items.menu_items;
         itemsCtrl.category = items.category;
-        console.log("itemsCtrl.items",itemsCtrl.items)
     }
 })();
